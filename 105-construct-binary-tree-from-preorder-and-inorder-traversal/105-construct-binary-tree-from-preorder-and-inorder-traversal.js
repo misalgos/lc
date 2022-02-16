903722170bd2b32f8305b07d5eaddd09c474a7df
misalgos <99750691+misalgos@users.noreply.github.com>
2022-02-15 21:36:52 -0500

    Time: 125 ms (67.93%), Space: 44.7 MB (64.13%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    const map = new Map();
+    inorder.forEach((e,i)=> {
+        map.set(e, i);
+    });
+    function build(start, end) {
+        if(start > end) return null;
+        let root = new TreeNode(preorder.shift());
+        root.left = build(start, map.get(root.val)-1);
+        root.right = build(map.get(root.val)+1, end);
+        return root;
+    }
+    return build(0, inorder.length-1);
+};
+
+
